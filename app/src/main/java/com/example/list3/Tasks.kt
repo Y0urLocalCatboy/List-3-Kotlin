13442a2199bf6fa1d9fa697a67e8c8cd50638b2f
Y0urLocalCatboy <156523842+Y0urLocalCatboy@users.noreply.github.com>
2024-10-27 19:24:20 +0100

    Last commit (hopefully)

--- app/src/main/java/com/example/list3/Tasks.kt
+++ app/src/main/java/com/example/list3/Tasks.kt
@@ -12,19 +12,13 @@ class Fraction{
         this.denominator = denominator
     }
     fun reduce(): Fraction{
-        var a = this.numerator
-        var b = this.denominator
-        while (b != 0){
-            val t = b
-            b = a % b
-            a = t
-        }
-        return(Fraction(this.numerator, this.denominator))
+        val a = this.greatestCommonDivisor() //OH! Initially I had LITERALLY the same code in greatestCommonDivisor() and reduce() functions
+        return(Fraction(this.numerator/a, this.denominator/a))
     }
     fun greatestCommonDivisor(): Int{
         var a = this.numerator
         var b = this.denominator
-        while (b != 0){
+        while (b != 0){ //this algorithm is pulled from matemaks and translated to Kotlin by me
             val t = b
             b = a % b
             a = t
@@ -39,12 +33,13 @@ class Fraction{
     }
 }
 fun main(){
-    val f1 = Fraction(3, 5)
+    val f1 = Fraction(2, 4)
     val f2 = Fraction(3)
-    val f3 = Fraction(4, 5)
-    println(f1.reduce().fractionToString())
-    println(f1.greatestCommonDivisor())
-    println(f2.fractionToString())
-    println(f3.fractionToString())
-    println(f1.quot(f2).fractionToString())
+    val f3 = Fraction(30, 45)
+    println("f1 = ${f1.fractionToString()}")
+    println("f2 = ${f2.fractionToString()}")
+    println("f3 = ${f3.fractionToString()}")
+    println("f1 * f2 = ${f1.quot(f2).fractionToString()} = ${f1.quot(f2).reduce().fractionToString()}")
+    println("f3 reduced = ${f3.reduce().fractionToString()}")
+    println("Biggest common divisor of f3 = ${f3.greatestCommonDivisor()}")
 }
